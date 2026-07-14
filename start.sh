a3843dc5f718ccd8e03acad38b06ba4db414d5cf
baharjun6 <baharjun0609@gmail.com>
2026-07-15 01:07:51 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,27 +2,17 @@
 
 set -e
 
-
 export UVICORN_HOST=0.0.0.0
 export UVICORN_PORT=${PORT:-8000}
 
-
 export XRAY_JSON=/code/xray_config.json
-
 
 export SQLALCHEMY_DATABASE_URL=sqlite:////code/marzban.db
 
-
-if [ -n "$SUDO_USERNAME" ] && [ -n "$SUDO_PASSWORD" ]; then
-
-python3 -m marzban.cli admin create \
--u "$SUDO_USERNAME" \
--p "$SUDO_PASSWORD" || true
-
-fi
-
+echo "Running database migration..."
 
 alembic upgrade head
 
+echo "Starting Marzban..."
 
 python3 main.py
